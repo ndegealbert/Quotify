c8358e2ff32d3e98519410ccf54135b9fd5b73ec
albert <ndegealbert>
2020-09-24 03:00:27 -0400

    initial  commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quotify  from  './componet/quotify'
+
+function App() {
+  return (
+    <div className="App">
+      <Quotify />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componet/quotify.js
@@ -0,0 +1,11 @@
+import  React  from  'react'
+import  quote  from  "./../json/quote.json"
+import  author  from  "./../json/authors.json"
+export default  function  Quotify(){
+    console.log(quote)
+    console.log("-----------------")
+    console.log(author)
+    return <div>
+        <p>Hello  Word</p>
+    </div>
+}
