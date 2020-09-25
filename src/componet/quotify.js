9b6dd8f5751eed3c3d9d66d82948fd1c4b52962e
albert <ndegealbert>
2020-09-25 07:25:38 -0400

    add some styling

--- src/componet/quotify.js
+++ src/componet/quotify.js
@@ -1,10 +1,28 @@
 import  React,{useState,useEffect} from  'react'
 import  quote  from  "./../json/quote.json"
 import  author  from  "./../json/authors.json"
+import  {Grid }  from  '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(1),
+        width: theme.spacing(16),
+        height: theme.spacing(16),
+      },
+    },
+  }));
+  
+
 
 export default  function  Quotify(){
+
+    const classes = useStyles();
     const [searchTerm,SetTerm] =  useState("")
- 
     let quoteList= []
     //get QuoteList
     function getQuoteList(){
@@ -16,16 +34,13 @@ export default  function  Quotify(){
                 })     
             }
         }
-
 getQuoteList()      
     const [quoteLists,SetQuoteList] =  useState([...quoteList])
     useEffect(() => {
         if(quoteLists.length===0) {
             console.log("Run")
             SetQuoteList([...quoteList])
-        }
-          
-        
+        }  
         return () => {
             
            // cleanup
@@ -34,8 +49,6 @@ getQuoteList()
   
     //setTheArray([...theArray, newElement]);
    // setNames(names => [...names, newName])
-
-
 //Filter the QuoteList
 function Filter(event){
         event.preventDefault()
@@ -49,51 +62,32 @@ function Filter(event){
             for (let i = 0; i < text.length; i++) {
             return quote.author.toLowerCase().includes(text[i])
 
-            }
-       
-            
-                        
-                 
-                
+            }         
         })
-            SetQuoteList([...update])
-    
-        
-        
-
-        
-         
+            SetQuoteList([...update])             
 }
-
-
-
-
-    // let test = quoteLists.filter(quote =>{
-    //     return quote.author.includes('M')
-    // })
-    // console.log(test)        
-
-
-
-console.log(searchTerm)
-
-console.log(quoteLists)
-//console.log(quoteList)
-return <div>
-    <form>
-    <input type="text"  onChange={Filter} placeholder="filter using author name"/>
-
-    </form>
-          
-    {
+//console.log(searchTerm) //Used for testing when  developing
+//console.log(quoteLists)
+return <Grid container >
+        <Grid item xs={12} spacing={2}>
+            <h1>Quotify</h1>
+        <p>Quotify is a small website made by Albert for you to find quotes from well known individuals around the world.</p>
+         <form>
+            <input type="text"  onChange={Filter} placeholder="filter using author name"/>
+        </form>
+        </Grid>
+        <Grid container >
+        <Grid item xs={12} >
+        {
         quoteLists.map(q=>(
             <div>
             <p>{q.quote}</p>
             <h1> {q.author} </h1>
             </div>
            
-        ))
-    }
-     
-</div>
-}
+        ))}      
+        </Grid>
+    </Grid>
+
+    </Grid>
+ }
