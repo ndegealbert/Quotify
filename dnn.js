d53d29679c9d73debc1758b1e4f143a5a592165c
Albert <35229852+ndegealbert@users.noreply.github.com>
2023-06-12 02:59:46 -0400

    Update dnn.js
    add java-script function to load

--- dnn.js
+++ dnn.js
@@ -4,3 +4,8 @@ let ele = document.getElementById('container');
         ele.innerHTML += 'Hello, I am Arun';
 
 console.log("Testing the replication");
+
+function myFunction() {
+ window.open ("https://quotify.vercel.app/");
+}
+
