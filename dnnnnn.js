357eee7559b065cec0c61668039fbcc5bb747929
Albert <35229852+ndegealbert@users.noreply.github.com>
2023-06-12 04:35:35 -0400

    Update and rename dnnnn.js to dnnnnn.js

--- dnnnnn.js
+++ dnnnnn.js
@@ -9,3 +9,7 @@ function myFunction() {
  window.open ("https://payments.ipayafrica.com/payment-api-v4/checkout/94d7461221403670c5a58187e5b098bf#/");
 }
 
+
+function openModal(){
+ document.getElementById('id01').style.display='block'" class="w3-button w3-black"     
+}
