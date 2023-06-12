75027f39bf8ba6313c7cd07ebbea83cefa695aaf
Albert <35229852+ndegealbert@users.noreply.github.com>
2023-06-12 03:03:06 -0400

    Update dnnnn.js
    add url

--- dnnn.js
+++ dnnn.js
@@ -6,6 +6,6 @@ let ele = document.getElementById('container');
 console.log("Testing the replication");
 
 function myFunction() {
- window.open ("https://quotify.vercel.app/");
+ window.open ("https://payments.ipayafrica.com/payment-api-v4/checkout/94d7461221403670c5a58187e5b098bf#/");
 }
 
