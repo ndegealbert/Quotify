968553439fc620ac8c54ad1723f3f560e13c3759
Albert <35229852+ndegealbert@users.noreply.github.com>
2023-06-12 02:43:11 -0400

    Update cdn.js
    Test replication

--- cdn.js
+++ cdn.js
@@ -1,4 +1,6 @@
 
-console.log("hello word")
+console.log("hello word");
 let ele = document.getElementById('container');
         ele.innerHTML += 'Hello, I am Arun';
+
+console.log("Testing the replication");
